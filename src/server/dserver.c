d590866d4efceb416e8f82f8232159fc52225950
a106936 <a106936@alunos.uminho.pt>
2025-04-07 16:51:58 +0100

    Correção

--- src/server/dserver.c
+++ src/server/dserver.c
@@ -24,28 +24,29 @@ int main() {
         close(fd);
 
         if (bytes > 0) {
-            print_message(msg);
+            //print_message(msg);
            
             if (verifica_comando(msg) == 1) {
-               printf("Comando válido\n");
+               //printf("Comando válido\n");
                 
+               if(get_message_command(msg)=='a'){
+                MetaDados *data = create_metaDados(msg);
+                //print_metaDados(data);
+    
+                //Guardar os metadados na estrutura de dados
+    
+                fd = open(SERVER_FIFO, O_WRONLY);
+                write(fd,get_MD_key(data),512);
+                close(fd);
+    
+            }
                
             } else {
                 error_message(get_message_command(msg)); // isto terá de ser alterado para um char
             }
         }
 
-        if(get_message_command(msg)=='a'){
-            MetaDados *data = create_metaDados(msg);
-            //print_metaDados(data);
-
-            //Guardar os metadados na estrutura de dados
-
-            fd = open(SERVER_FIFO, O_WRONLY);
-            write(fd,get_MD_key(data),512);
-            close(fd);
-
-        }
+        
     }
 
     printf("[SERVER ENDED]\n");
