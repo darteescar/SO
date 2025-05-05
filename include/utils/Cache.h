a1952471a2e4e60d09d3be083b497ab0e378d268
a106932 <a106932@alunos.uminho.pt>
2025-05-05 17:15:27 +0100

    Resto da documentação

--- include/utils/Cache.h
+++ include/utils/Cache.h
@@ -26,46 +26,175 @@
 
 typedef struct cache Cache;
 
+/**
+ * @brief Aloca memória para uma estrutura de dados do tipo Cache
+ * 
+ * @param max_docs Tamanho máximo da cache
+ * @param flag Flag que indica se a cache é estática ou dinâmica(0 se for estática ou 1 se for dinâmica)
+ * @return Cache* Apontador para a estrutura de dados alocada
+ */
 Cache *create_Cache(int max_docs, int flag);
 
+/**
+ * @brief Função que adiciona um MetaDados à cache
+ * 
+ * @param cache Apontador para a estrutura de dados Cache
+ * @param data Apontador para a estrutura de dados MetaDados
+ * @param pos_onde_foi_add Flag que indica a posição onde o MetaDados foi adicionado
+ * @return Cache* Apontador para a estrutura de dados
+ * @note Se a cache estiver cheia, o MetaDados será adicionado ao disco
+ * @note Se a cache não estiver cheia, o MetaDados será adicionado à cache
+ * @note Se a cache for dinâmica, o MetaDados será adicionado à cache
+ */
 Cache *add_documento(Cache *cache, MetaDados *data, int *pos_onde_foi_add);
 
+/**
+ * @brief Função que adiciona um MetaDados à cache de forma estática
+ * 
+ * @param docs Apontador para a estrutura de dados Cache
+ * @param data Apontador para a estrutura de dados MetaDados
+ * @param pos_onde_foi_add Flag que indica a posição onde o MetaDados foi adicionado
+ * @return Cache* Apontador para a estrutura de dados
+ */
 Cache *add_documento_Estaticamente(Cache *docs, MetaDados *data, int *pos_onde_foi_add);
 
+/**
+ * @brief Função que adiciona um MetaDados à cache de forma dinâmica
+ * 
+ * @param docs Apontador para a estrutura de dados Cache
+ * @param data Apontador para a estrutura de dados MetaDados
+ * @param pos_onde_foi_add Flag que indica a posição onde o MetaDados foi adicionado
+ * @return Cache* Apontador para a estrutura de dados
+ */
 Cache *add_documento_Dinamicamente(Cache *docs, MetaDados *data, int *pos_onde_foi_add);
 
+/**
+ * @brief Função que adiciona um MetaDados à cache numa dada posição
+ * 
+ * @param cache Apontador para a estrutura de dados Cache
+ * @param data Apontador para a estrutura de dados MetaDados
+ * @param pos Posição onde o MetaDados será adicionado
+ */
 void add_to_Cache(Cache *cache, MetaDados *data, int pos);
 
+/**
+ * @brief Função que adiciona um MetaDados ao ficheiro em disco
+ * 
+ * @param cache Apontador para a estrutura de dados Cache
+ * @param data Apontador para a estrutura de dados MetaDados
+ */
 void add_to_Disk(Cache *cache, MetaDados *data);
 
-void add_to_Disk(Cache *cache, MetaDados *data);
-
+/**
+ * @brief Função que remove um MetaDados da cache
+ * 
+ * @param cache Apontador para a estrutura de dados Cache
+ * @param pos Posição do MetaDados a ser removido
+ * @return Cache* Apontador para a estrutura de dados Cache
+ */
 Cache *remove_file (Cache *cache, int pos);
 
+/**
+ * @brief Função que retorna uma string com as informações do documento com uma dada key
+ * 
+ * @param cache Apontador para a estrutura de dados Cache
+ * @param pos key
+ * @return char* String com as informações do documento
+ * @note Se a informação desse documento não estiver na cache, a função irá procurar no disco
+ */
 char *consult_file (Cache *cache, int pos);
 
+/**
+ * @brief Função que liberta a memória alocada para a estrutura de dados Cache
+ * 
+ * @param docs Apontador para a estrutura de dados Cache
+ */
 void free_Cache(Cache *docs);
 
+/**
+ * @brief Função que redimensiona o array de ocupados da cache
+ * 
+ * @param docs Apontador para a estrutura de dados Cache
+ */
 void redimensiona_ocupados(Cache *docs);
- 
+
+/**
+ * @brief Função que redimensiona os arrays de ocupados e stack da cache
+ * 
+ * @param docs Apontador para a estrutura de dados Cache
+ */
 void redimensionar_auxiliares(Cache *docs);
 
+/**
+ * @brief Função que verifica se a informação de um documento existe na cache
+ * 
+ * @param docs Apontador para a estrutura de dados Cache
+ * @param pos Key do documento a ser verificado
+ * @return int 1 se a informação do documento existe, -1 se a posição for inválida, -2 se a informação do documento não existir
+ */
 int documento_existe(Cache *docs, int pos);
 
+/**
+ * @brief Função que retorna o estado de um MetaDados na cache
+ * 
+ * @param docs Apontador para a estrutura de dados Cache
+ * @param pos Key dos MetaDados a ser verificado
+ * @return char Estado do documento na cache
+ */
 char get_docs_estado(Cache *docs, int pos);
 
+/**
+ * @brief Função que retorna o MetaDados na cache ou no disco
+ * 
+ * @param docs Apontador para a estrutura de dados Cache
+ * @param pos Key do documento a retornar
+ * @return MetaDados* Apontador para a estrutura de dados MetaDados
+ */
 MetaDados *get_anywhere_documento(Cache *docs, int pos);
 
+/**
+ * @brief Função que imprime os MetaDados na cache
+ * 
+ * @param docs Apontador para a estrutura de dados Cache
+ */
 void print_Cache (Cache *docs);
 
+/**
+ * @brief Função que retorna o size da cache
+ * 
+ * @param docs Apontador para a estrutura de dados Cache
+ * @return int Size da cache
+ */
 int get_Max_docs (Cache *docs);
 
+/**
+ * @brief Função que escreve todos os MetaDados da cache para o disco
+ * 
+ * @param docs Apontador para a estrutura de dados Cache
+ */
 void all_Cache_to_Disc (Cache *docs);
 
+/**
+ * @brief Função que desserializa um MetaDados a partir do disco
+ * 
+ * @param pos Posição do MetaDados a ser desserializado
+ * @return MetaDados* Apontador para a estrutura de dados MetaDados
+ */
 MetaDados* desserializa_MetaDados(int pos);
 
+/**
+ * @brief Função que retorna a flag da cache
+ * 
+ * @param docs Apontador para a estrutura de dados Cache
+ * @return int Flag da cache(0 se for estática, 1 se for dinâmica)
+ */
 int get_cache_flag(Cache *docs);
 
+/**
+ * @brief Função que recupera o backup da cache a partir do disco
+ * 
+ * @param docs Apontador para a estrutura de dados Cache
+ */
 void recupera_backup(Cache *docs);
 
 #endif
--- include/utils/Metadados.h
+++ include/utils/Metadados.h
@@ -26,42 +26,149 @@
 
 typedef struct metaDados MetaDados;
 
+/**
+ * @brief Aloca memória para uma estrutura de dados do tipo MeatDados
+ * 
+ * @return MetaDados* Apontador para a estrutura de dados alocada
+ */
 MetaDados *init_MD();
 
 MetaDados *create_MD_1 (int pid , char *buffer);
 
+/**
+ * @brief Cria uma estrutura de dados do tipo MetaDados a partir de um buffer
+ * 
+ * @param buffer Buffer contendo os dados a serem armazenados na estrutura
+ * @return MetaDados* Apontador para a estrutura de dados criada
+ */
 MetaDados *criar_metaDados(char *buffer);
 
+/**
+ * @brief Função que retorna o caminho do arquivo armazenado na estrutura de dados
+ * 
+ * @param data Apontador para a estrutura de dados
+ * @return char* Caminho do arquivo armazenado na estrutura
+ */
 char* get_MD_path(MetaDados *data);
 
+/**
+ * @brief Função que retorna a key do MetaDados armazenado na estrutura de dados
+ * 
+ * @param data Apontador para a estrutura de dados
+ * @return int Key do MetaDados armazenado na estrutura
+ */
 int get_MD_pos_in_disk(MetaDados *data);
 
+/**
+ * @brief Função que define a posição do MetaDados armazenado na estrutura de dados
+ * 
+ * @param data Apontador para a estrutura de dados
+ * @param pos Posição a ser definida
+ */
 void set_MD_disk_position(MetaDados *data, int pos);
 
+/**
+ * @brief Função que retorna uma string com os dados do MetaDados correspondente a uma dada key
+ * 
+ * @param data Apontador para a estrutura de dados
+ * @param key Key do MetaDados armazenado na estrutura
+ * @return char* String com os dados do MetaDados
+ */
 char *MD_toString(MetaDados* data, int key);
 
+/**
+ * @brief Função que converte os dados do MetaDados para uma string no formato de disco
+ * 
+ * @param data Apontador para a estrutura de dados
+ * @return char* String com os dados do MetaDados no formato de disco
+ */
 char *to_disk_format_MD(MetaDados *data);
 
+/**
+ * @brief Função que imprime os dados do MetaDados
+ * 
+ * @param data Apontador para a estrutura de dados
+ */
 void print_MD(MetaDados *data);
 
+/**
+ * @brief Função que converte uma string no formato de disco para um buffer que pode ser usado para criar MetaDados
+ * 
+ * @param data Apontador para a estrutura de dados
+ * @return char* String no formato usado na função criar_metaDados
+ */
 char* from_disk_format_MD(char *data);
 
+/**
+ * @brief Função que retorna o tamanho da estrutura de dados MetaDados
+ * 
+ * @param data Apontador para a estrutura de dados
+ * @return int Tamanho da estrutura de dados
+ */
 int get_MD_size (MetaDados *data);
 
+/**
+ * @brief Função que retorna o comando present no buffer armazenado na estrutura de dados
+ * 
+ * @param msg Apontador para a estrutura de dados
+ * @return char Comando armazenado no buffer
+ */
 char get_MD_command(MetaDados *msg);
 
+/**
+ * @brief Função que retorna o buffer armazenado na estrutura de dados
+ * 
+ * @param msg Apontador para a estrutura de dados
+ * @return char* Buffer armazenado na estrutura
+ */
 char *get_MD_buffer(MetaDados *msg);
 
+/**
+ * @brief Função que retorna o PID armazenado na estrutura de dados
+ * 
+ * @param msg Apontador para a estrutura de dados
+ * @return int PID armazenado na estrutura
+ */
 int get_MD_pid(MetaDados *msg);
 
+/**
+ * @brief Função que retorna a palavra-chave armazenada no buffer da estrutura de dados
+ * 
+ * @param msg Apontador para a estrutura de dados
+ * @return char* Palavra-chave armazenada no buffer
+ */
 char *get_MD_keyword(MetaDados *msg);
 
+/**
+ * @brief Função que define o PID armazenado na estrutura de dados
+ * 
+ * @param msg Apontador para a estrutura de dados
+ * @param pid PID a ser definido
+ */
 void set_MD_pid(MetaDados *msg, int pid);
 
+/**
+ * @brief Função que define o buffer armazenado na estrutura de dados
+ * 
+ * @param msg Apontador para a estrutura de dados
+ * @param buffer Buffer a ser definido
+ */
 void set_MD_buffer(MetaDados *msg, char *buffer);
 
+/**
+ * @brief Função que libera a memória alocada para a estrutura de dados
+ * 
+ * @param data Apontador para a estrutura de dados
+ */
 void free_MD(MetaDados *data);
 
+/** 
+ * @brief Função que retorna o valor de um campo específico armazenado na estrutura de dados
+ * 
+ * @param msg Apontador para a estrutura de dados
+ * @param n Número do campo a ser retornado
+ * @return char* Valor do campo armazenado na estrutura
+ */
 char *get_MD_something (MetaDados *msg, int n);
 
 #endif
